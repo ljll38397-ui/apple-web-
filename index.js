d902a96696290bab9a39876bf8c1d890809619ee
ljll38397-ui <ljl38397@gmail.com>
2026-07-30 16:23:58 +0800

    vrgeljlel

---
+++ index.js
@@ -0,0 +1,639 @@
+// let a = 1;
+// let b = -5;
+// let c = 6;
+// let d = 5 * 5 - 4 * 1 * 6;
+// console.log(d);
+// let x = -5 + Math.sqrt5 * 5 - ((4 * 1 * 6) / 2) * 1;
+// let a = 2;
+// let b = 5;
+// let c = -3;
+// let x1 = (-5 - Math.sqrt(5 * 5 - 4 * 2 * -3)) / (2 * 2);
+// let x2 = (-5 + Math.sqrt(5 * 5 - 4 * 2 * -3)) / (2 * 2);
+// console.log(x1, x2);
+// let a = 1;
+// let b = 6;
+// let c = 8;
+// let x1 = (-6 - Math.sqrt(6 * 6 - 4 * 1 * 8)) / (2 * 1);
+// let x2 = (-6 + Math.sqrt(6 * 6 - 4 * 1 * 8)) / (2 * 1);
+// console.log(x1, x2);
+// let age = 18;
+// if (age == 21) {
+//   console.log("ta nasand hvrsen");
+// } else {
+//   console.log("ta nasand hvreegvi");
+// }
+// let nas = 18;
+// if (nas <= 22) {
+//   console.log("ta bol oyutan");
+// } else {
+//   console.log("ta bol suragch");
+// }-
+// let password = "admin123";
+// if (password == "admin123") {
+//   console.log("amjilttai nevterlee");
+// } else {
+//   ("password buruu bn");
+// }
+// let nas = 18;
+// if (nas >= 21) {
+//   console.log("baarand orj bolno");
+// } else if (nas <= 18) {
+//   console.log(" nasand hvreeegvi bn");
+// } else {
+//   console.log(" Vnemlehee shalguulnuu");
+// }
+// let nas = 16;
+// if (nas <= 17) {
+//   console.log("hvvhed");
+// } else if (nas <= 22) {
+//   console.log("oyutan");
+// } else if (nas <= 50) {
+//   console.log("tom hvn");
+// } else {
+//   console.log("ondor nastan");
+// }
+// let a = 5;
+// let b = 8;
+// if (a <= b) {
+//   console.log("b");
+// } else {
+//   console.log("a");
+// }
+// let temp = 17;
+// if (temp >= 30) {
+//   console.log("hot");
+// } else if (temp >= 20) {
+//   console.log("dulaahan");
+// } else {
+//   console.log("hviten");
+// }
+// let year = 1100;
+// if (year % 400 == 0) {
+//   console.log("ondor jil");
+// } else if (year % 100 == 0) {
+//   console.log("ondor jil bish");
+// } else if (year % 4 == 0) {
+//   console.log("ondor jil");
+// } else {
+//   console.log(" on oruulna uu");
+// }
+// let i = 0;
+// for (let i = 0; i < 5; i++) {
+//   console.log(i + "");}
+// let count = 0;
+// while (count < 3) {
+//   console.log("1" + count);
+//   count++;
+// }
+// let isweaterGood=false;
+// let havefriends=false
+// let isweaterGood = false;
+// let havefriends = true;
+// if (isweaterGood && havefriends) {
+//   console.log("holbombog toglono");
+// } else if (isweaterGood || havefriends) {
+//   console.log("gertei toglono");
+// } else {
+//   console.log("hicheelei hii");
+// // }
+// let age = 18;
+// let haveTicket = false;
+// let parentsAgreed = false;
+// if (haveTicket && parentsAgreed) {
+//   console.log(" toglolt vzne");
+// } else if (haveTicket || parentsAgreed) {c
+//   console.log(" aaw eejin zowshooroltei vzne");
+// } else {
+//   console.log("toglolt vzehgvi");
+// }
+// let username = false;
+// let password = false;
+// let blockoo = false;
+// if (username && password) {
+//   console.log("nevterlee");
+// } else if (username || password) {
+//   console.log("blockoo garguulna uu");
+// } else {
+//   console.log("username or password wrong");
+// }
+// let i = 0;
+// // for (let i = 0; i <= 10; i++) console.log(i);
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+// for (let i = 3; i <= 9; i = i + 3) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 100; i += 2) {
+//   console.log(i);
+// // }
+// let = 1;
+// for (let i = 1; i <= 100; i += 1) {
+//   console.log(i);
+// }
+// if (i % 2 == 1) {
+//   console.log("sondgoi too");
+// } else if (i % 4 == 0) {
+//   console.log("tegsh too");}
+// let star = "*";
+// for (let i = 1; i <= 5; i++)
+//   for (let j = 1; j <= 5; i++) {
+//     console.log(star);
+//   }
+// let n = 5;
+// for (n = 1; n <= 5; n++) {
+//   console.log(n);
+// // }
+// let star = "*";
+// for (i = 0; i <= 5; i++);
+// {
+//   console.log(star);
+// }
+// let a = 4;
+// let b = 16;
+// for (i = 4; i <= 100; i++)
+//   if (i % a == 0)
+//     console.log("huvaagdag too", i);
+//   } else {
+//     console.log("huvaagdagvi ");
+//   }
+// let b = 16;
+// for (i = 1; i <= 100; i++)
+//   if (i % b == 0) {
+//     console.log("huvaagdaggvi", i);
+//   }
+// let string = "hello world";
+// console.log(string[2], string[4], string[6]);
+// let string = "hello world";
+// console.log(string.toLocaleUpperCase());
+// let b;
+// for (i = 4; i <= 100; i++)
+//   if (i % 16 == 0) {
+//     console.log("huvaagdaggvi", i);
+//   }
+//  let username = "lhagva";
+// let i = 6;
+//  if (i > 3) {
+//   console.log("3aas ih");
+//  } else {
+//   console.log("3aas bag");
+// }
+// let password = 2026047;
+//  let a = 8;
+//  if (a > 7) {
+//   console.log("8aas bag");
+// } else {
+//    console.log("8aas ih");
+// }
+// let password = "2026099";
+// if (password.length < 8) console.log("password is too short");
+// else {
+//   console.log("password length is okey");
+// }
+// let pass = "tempo";
+// let number = /\d/.test(pass);
+// if (number) {
+//   console.log("too orson bn");
+// } else {
+//   console.log("too oroogvii bn");
+// // }
+// for (i = 0; i <= 20; i++)
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// let n = 10;
+// let sum = 0;
+// for (i = 0; i <= n; i++)
+//   if ((sum = sum + i)) {
+//     console.log(sum);
+//   }
+// let n = 5;
+// for (i = 1; i <= n; i++) {
+//   console.log(i);
+// // }
+// let n = 7;
+// for (i = 0; i < n; i++) {
+//   console.log(n);
+//
+// for (i = 0; i <= 10; i++) {
+//   if (i == 5) continue;
+//   console.log(i);
+// }
+// let n = 10;
+// let sum = 0;
+// for (i = 1; i <= n; i++) {
+//   if ((sum = sum + i)) console.log(sum);
+// }
+// let string = "hello world";
+// console.log(string[2]);
+//144-->44
+//123-->0
+//1234666-->66
+// let n = 5;
+// for (i = 1; i < 11; i++) {
+//   console.log(n, "*", i, "=", n * i);
+// }
+// let string = "hello";
+// {
+//   console.log(reverse[0], reverse[1], reverse[2], reverse[3], reverse[4]);
+// }
+// for (i = 4; i >= 0; i--) {
+//   console.log(string[i]);
+// }
+// for (i = 4; i >= string.length; i--) {
+//   console.log(string[i]);
+// }
+// for (i = 0; i <= string.length; i++) {
+//   console.log(string[i]);
+// let star = "";
+// star = star + " * ";
+// star = star + " * ";
+// star = star + " * ";
+// star = star + " * ";
+// console.log(star);
+// let stars = "";
+// stars = stars + " * ";
+// console.log(stars);
+// let star = "";
+// for (i = 1; i <= 5; i++) {
+//   star = star + "*";
+//   console.log(star);
+// }
+// let star = "";
+// for (i = 0; i <= 5; i++) star = star + " * ";
+// {
+//   for (j = 0; j <= 5; j++) console.log(star);
+// // }
+// let a = "";
+// let b = "";
+// for (i = 1; i <= 5; i++) {
+//   if (i == 3) {
+//     a = " ";
+//   } else {
+//     a = "*";
+//   }
+//   b = b + a;
+// }
+// // console.log(b);
+// let star = "";
+// star = star + "*";
+// star = star + "-";
+// star = star + "*";
+// console.log(star);
+
+// //<-- array-->
+// let names = ["bat", "bold", "dondog"];
+// let hobbys = [
+//   "daam togloh",
+//   "mori unah",
+//   "usand seleh",
+//   "sags togloh",
+//   "uuland avirah",
+//];
+// // console.log(hobbys[2]);
+// console.log(hobbys[3], hobbys[4]);
+// console.log(hobbys.length);
+
+// for (i = 0; i < hobbys.length; i++) {
+//   console.log(hobbys[i]);
+// let ages = [
+//   28, 36, 30, 32, 23, 41, 38, 21, 36, 22, 23, 23, 22, 22, 21, 44, 25, 24,
+// ];
+
+// for (i = 0; i < ages.length; i++);
+// {
+//   console.log(ages[i]);
+// }
+// console.log(ages[2]);
+// let sum = 0;
+// let dundaj = 0;
+// for (i = 0; i < ages.length; i++) {
+//   sum = sum + ages[i];
+//   console.log(sum);
+// }
+// dundaj = sum / ages.length;
+// {
+//   console.log(dundaj);
+// // }
+// for (i = 0; i < ages.length; i++) {
+// //   if (ages[i] >= 25) console.log(ages[i]);
+// let sum = 0;
+// for (i = 0; i < ages.length; i++) {
+//   if (ages[i] >= 20);
+//   if (ages[i] <= 29) console.log(ages[i]);
+// }
+// for (i = 0; i < ages.length; i++) {
+//   sum = sum + ages[i];
+//   console.log(sum);
+// }
+
+// // for (i = 0; i < ages.length; i++) {
+// //   if (ages[i] >= 30);
+// //   if (ages[i] <= 39) console.log(ages[i]);
+// // // }
+// let min = ages[0];
+// let sum = 0;
+// for (i = 0; i < ages.lengt; i++) {
+//   if (min > ages[i]);
+//   {
+//     min = ages[i];
+//   }
+//   sum = sum + ages[i];
+//   console.log(min);
+// // // }
+// let array = [1, 2, 3, 4];
+// console.log(array);
+// let a = [
+//   "Чингүн",
+//   "Lkhagvasuren",
+//   "Ууганбаяр",
+//   "Наргис",
+//   "Билэгүүдэй",
+//   "Шинэбаяр",
+//   "Золбаяр",
+//   "Жавхлан",
+//   "Балжинням",
+//   "Хатанбаатар",
+//   "Батбаяр",
+//   "Төгсбилэг",
+//   "Баярмаа",
+//   "Булганбат",
+//   "Мягмарсүрэн",
+//   "НандинЭрдэнэ",
+//   "Бямбасайхан",
+//   "Нандин-Эрдэнэ",
+//   "Цэнгүүн",
+//   "Отгонжаргал",
+//   "Оюунэрдэнэ",
+//   "Лхагвадорж",
+//   "мягмарсүрэн",
+//   "Өсөхбаяр",
+// ];
+// let b = [];
+
+// for (i = 0; i < a.length; i++) {
+//   console.log(a[i]);
+// }
+// for (i = 0; i < a.length; i++)
+//   if (i % 2 == 0) {
+//     // b.push(a[i]);
+//     // console.log(a[i]);
+//   }
+// console.log(b[i]);
+// // let towerOne=["red","blue","red","green"]
+// // // let towerTwo=["blue","green","blue",""]
+// // //<--push-->
+// let odd = [1, 3, 5, 7, 9];
+// let even = [2, 4, 6, 8, 10];
+// let max = odd[0];
+// let min = odd[0];
+
+// for (i = 0; i < even.length; i++) {
+//   odd.push(even[i]);
+//   console.log(even[i]);
+// }
+// console.log(odd[i]);
+// for (i = 0; i < odd.length; i++) {
+//   max > odd[i];
+//   max = odd[i];
+// }
+// // console.log(max);
+// let array = [1, 2, 3, 4, 5];
+// // array[8] = array[2];
+// // console.log(array);
+// let a = [1, 3, 5, 7];
+// let b = [2, 4, 6];
+// for (i = 0; i < b.length; i++) a.push(b[i]);
+// console.log(b[i]);
+// let number = [19, 20, 31, 88, 24, 10, 8, 39, 1, 5];
+// let max = number[0];
+// let max2 = number[0];
+// for (i = 0; i < number.length; i++) if (max < number[i]) max = number[i];
+// {
+//   console.log(max);
+// }
+// for (i = 0; i < number.length; i++) {
+//   max - number.length;
+// // }
+// // console.log(max2);
+//<----function---->
+// function toonuud(a, b) {
+//   console.log(a + b);
+//   console.log(a - b);
+//   console.log(a / b);
+//   console.log(a * b);
+// }
+// toonuud(10, 10);
+// toonuud(10, 10);
+// toonuud(20, 5);
+// toonuud(20, 5);
+// function printtext() {
+//   console.log("hello world");
+// }
+// printtext();
+// function printlastchar(text) {
+//   console.log(text[text.length - 1]);
+// }
+// printlastchar("hello world");
+// function too(a, b) {
+//   console.log(a + b);
+// }
+// too(10, 10);
+// function toohasah(a, b) {
+//   console.log(a - b);
+// }
+// toohasah(10, 10);
+// function toohuvaah(a, b) {
+//   console.log(a / b);
+// }
+// toohuvaah(10, 2);
+// let name = prompt("ta neree oruuln uu");
+// console.log(name);
+// let nas = prompt(`${name} ta nasaa oruuln uu`);
+// console.log(nas);
+
+// let a = prompt(`${name} ta a toog  oruuln uu`);
+// let b = prompt(`${name} ta b toog  oruuln uu`);
+// let op = prompt("ta ymar uildel hiimeer bn. etc(+ ,- ,* ,/) hiij bolno.");
+// if (add === "+") {
+//   add(a, b);
+// }
+// function add(a, b) {
+//   console.log(Number(a) + Number(b));
+// }
+// let message = document.getElementById("greeting");
+// message.textContent = "hello";
+// let text = document.getElementById("box");
+// text.innerHTML = "<h5>fhdgjhgfdka</h5>";
+// let hairtsag = document.getElementById("code");
+// hairtsag.innerHTML = "Амттан захиалах булан";
+// let hairtsag = document.getElementById("code");
+// // hairtsag.innerHTML = "Таны бялууны захиалгыг хүлээж авлаа";
+// let colors = red.textContent;
+// let red = document.getElementsByClassName("box")[0];
+// function redcolor() {
+// //   red.textContent = "51";
+// // // }
+// let chiiden11 = document.getElementById("chiiden");
+// function chiiden12() {
+//   chiiden11.innerHTML = "<img src='light-on.gif'/>";
+// }
+// function chiiden13() {
+//   chiiden11.innerHTML = "<img src='light-off.gif'/>";
+// }
+// let color = document.getElementById("box");
+// function bluecolor() {
+//   color.textContent = "tsenher ongo";
+//   color.style.color = "blue";
+// }
+// function redcolor() {
+//   color.textContent = "ulaan ongo";
+//   color.style.color = "red";
+// }
+// function blackcolor() {
+//   color.textContent = "har ongo";
+//   color.style.color = "black";
+// // }
+// let redwidth = 50;
+// let bluewidth = 50;
+// let red = document.getElementsByClassName("box")[0];
+// let blue = document.getElementsByClassName("box1")[0];
+// function redcolor() {
+//   redwidth++;
+// //   red.textContent = redwidth;
+// // }
+// let boxs = document.getElementById("task");
+// function btn() {
+//   boxs.textContent = "";
+// }
+
+// let todos = [
+//   { taskname: "usand seleh", isCompleted: false },
+//   { taskname: "uuland avirah", isCompleted: true },
+// ];
+
+// const todosContainer = document.getElementsByClassName("todos")[0];
+// for (i = 0; i < todos.length; i++) {
+//   let todo = todos[i];
+//   let html = `<div class="todo" >
+//    <div>${todo.taskname}
+//    </div>
+//    <input type="checkbox"/>
+//    </div>`;
+// }
+// todosContainer.innerHTML += html;
+//  let personas = [
+//   { name: "dorj", age: "23", phonenumber: "86765674", gender: "male" },
+// //   { name: "bat", age: "22", phonenumber: "95765674", gender: "male" },
+//   { name: "tuya", age: "24", phonenumber: "97755674", gender: "female" },
+//   { name: "enhee", age: "29", phonenumber: "89575574", gender: "male" },
+//   { name: "tsetseg", age: "27", phonenumber: "89575574", gender: "female" },
+// ];
+// for (i = 0; i < personas.length; i++) {
+//   if ("24" === personas[i].age) {
+//     console.log(personas[i], "monvv");
+//   }
+// }
+// let inputfield = document.createElement("input");
+// inputfield.type = "text";
+// inputfield.value = "sain bain uu";
+// document.body.appendChild(inputfield);
+// let selectmenu = document.createElement("select");
+// let option = document.createElement("option");
+
+// option.textContent = "Mongol";
+// option.value = "mn";
+// selectmenu.appendChild(option);
+// document.body.appendChild(selectmenu);
+// let person = [
+//   { firstName: "bold", lastName: "dorj", age: "25", city: "ulaanbaater" },
+// ];
+
+// for (i = 0; i < person.length; i++) {
+//   console.log(person[i].lastName);
+//   console.log(person[i].city);
+// }
+// let student = {
+//   name: "bat",
+//   age: "18",
+//   grades: { math: 85, science: 87, history: 100 },
+//   adress: { street: "3 gol office", city: "ulaanbaatar", zip: 12234 },
+// };
+// student.adress = "email:fdj@gmail.com";
+// console.log(student);
+// console.log(Object.keys(student));
+// console.log(Object.values(student));
+// // console.log(student);
+// // keys = Object.keys(student);
+// // values = Object.values(student);
+// // console.log(keys);
+// // console.log(values);
+// let todo = {
+//   title: "hello world",
+//   isdone: false,
+// };
+// todo.isdone = true;
+// console.log(todo);
+//<---x--->
+// let input = document.getElementById("input");
+// let btn1 = document.getElementById("btn1");
+// function click() {
+//   btn1.textContent = "grdhudfsh";
+//   console.log(btn1);
+// }
+//<-----tovchluur darahad ajil nemegdeh
+// let input = document.getElementById("input");
+// let ul = document.getElementById("ul");
+// function gargah() {
+//   let li = document.createElement("li");
+//   li.textContent = input.value;
+//   ul.appendChild(li);
+//   console.log(input.value, "input");
+//}
+//---------todolist----------------------------
+// let todos = [
+//   { taskname: "hool hiih", isCompleted: true },
+//   { taskname: "usand seleh", isCompleted: false },
+// ];
+
+// let input = document.getElementById("input");
+// let contact = document.getElementById("container");
+
+// function btn() {
+//   contact.innerHTML = "";
+
+//   for (i = 0; i < todos.length; i++) {
+//     let todo = todos[i];
+//     let taskdiv = document.createElement("div");
+//     taskdiv.innerHTML = todo.taskname;
+//     let checkbox = document.createElement("input");
+//     checkbox.type = "checkbox";
+//     taskdiv.appendChild(checkbox);
+//     checkbox.addEventListener("click", function () {
+//       todos[i].isCompleted = checkbox.checked;
+//     });
+
+//     contact.appendChild(taskdiv);
+//   }
+// }
+// function addtodo() {
+//   console.log("working....");
+//   todos.push({ taskname: input.value, isCompleted: false });
+//   btn();
+// }
+let todos = [
+  { taskname: "hool hiih", isCompleted: true },
+  { taskname: "uuland avirah", isCompleted: false },
+];
+let input = document.getElementById("input");
+let box = document.getElementById("box");
+function rendertodos() {
+  box.innerHTML = "";
+  for (let i = 0; i < todos.length; i++) {
+    let todo = todos[i];
+    let taskdiv = document.createElement("div");
+    taskdiv.innerHTML = todo.taskname;
+    console.log("todo", todo);
+  }
+}
